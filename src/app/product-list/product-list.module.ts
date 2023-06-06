a2ed07761c8642da26ffb785c7f40336e90318f5
Steven Boyd-Thompson <steveboydt@gmail.com>
2023-06-06 19:00:31 +0100

    - Install Angular Material - Add home component with placeholder text - Add product list module and add sample products to display - Add images to go with products - Update README

---
+++ src/app/product-list/product-list.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProductListRoutingModule } from './product-list-routing.module';
+import { ProductListComponent } from './product-list.component';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+
+@NgModule({
+  declarations: [
+    ProductListComponent
+  ],
+  imports: [
+    CommonModule,
+    ProductListRoutingModule,
+    MatListModule,
+    MatIconModule
+  ]
+})
+export class ProductListModule { }
